f68c029ace4dd24127cd57344b4a4e28739b05f0
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-10-02 21:13:38 +0100

    feat: added a background and loaded in game
    Background image was taken online, credits will be in README.

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ pygame.display.set_caption("Maze Game")
 
 character_image_sheet = pygame.image.load("assets\images\characters\Dude_Monster\Dude_Monster_Idle_4.png").convert_alpha()
 
+# Background image for the game
+cave_bg = pygame.image.load(r"assets\images\background\cave.png").convert_alpha()
+
 def game():
     # Game loop logic
     while True:
+        screen.blit(cave_bg, (0,0))
 
         # Event handling
         for event in pygame.event.get():
