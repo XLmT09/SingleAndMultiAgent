723744f55998010a90810f0ee9d1dad832948073
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-10-14 17:48:40 +0100

    feat: fill character map with walkable spots
    In the character map label any grid that can be walked on with a 1, also
    label any grid that can be climbed on with a 3.
    The character map will not show the diamond.

--- computer.py
+++ computer.py
@@ -24,9 +24,21 @@ class Computer:
         self.character = character
         self.maze_map = maze_map
         self.character_map = [[0 for _ in range(len(maze_map[0]))] for _ in range(len(maze_map))]
-        print(self.character_map)
+
+        self.fill_map_with_walkable_areas()
     
-    # def fill_map_with_walkable_areas():
+    def fill_map_with_walkable_areas(self):
+        for i in range(len(self.maze_map)):
+            for j in range(len(self.maze_map[0])):
+                if(self.maze_map[i][j] == 0):
+                    if ((i - 1 >= 0) and (j - 1 >= 0) and (i + 1 < len(self.maze_map)) and (j + 1 < len(self.maze_map[0]))):
+                        if ((self.maze_map[i-1][j] == 1) and (self.maze_map[i + 1][j] == 1)):
+                            self.character_map[i][j] = 1
+                if(self.maze_map[i][j] == 3):
+                    self.character_map[i][j] = 3
+        
+        print(*self.character_map, sep="\n")
+
 
 
     def move(self, screen, world_data, asset_groups, game_over):
