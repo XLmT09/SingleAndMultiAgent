495c8ce70aeebd51e3c555bc8c539703a785e9e3
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-10-29 15:28:29 +0000

    docs: Add comments for computer.py
    After modularising the computer class into Random and BFS, i've added
    comments to them.

--- product/computer.py
+++ product/computer.py
@@ -7,9 +7,22 @@ import numpy as np
 moves = ["LEFT", "RIGHT"]
 
 class Computer:
+    """ This class will represent the agent different pathfinding algos available to use.
+
+    Attributes:
+        character (CharacterAnimationManager): The character the computer will be controlling.
+        requested_movement (str): The movement the computer class will command the character to perfrom.
+        _walkable_maze_matrix (list of list): The maze which represents the walakable areas of the character.
+        _directions (list of tuple): The directions the computer can command the player to do.
+        stop_thread (bool): A flag to stop the path find algo thread.
+        th (Thread): The thread which will do the pathfinding.
+    
+    Args:
+        character (CharacterAnimationManager): The character the computer will be controlling.
+        walkable_maze (list of list): The maze which represents the walakable areas of the character.
+    """
     def __init__(self, character, walkable_maze):
         self.character = character
-        self.running = True
         self.requested_movement = "RIGHT"
         self._walkable_maze_matrix = walkable_maze
         # right, left, up, down
@@ -18,27 +31,35 @@ class Computer:
         self.th = threading.Thread(target=self.perfrom_path_find)
 
     def start_thread(self) -> None:
+        """ Start the path find thread. """
         self.th.start()
 
     def stop_path_find_algo_thread(self) -> None:
+        """ Stop the path find thread. """
         self.stop_thread = True
 
     def move(self, screen, world_data, asset_groups, game_over):
+        """ Move the character based on the requested movement. """
         return self.character.draw_animation(screen, world_data, asset_groups, game_over, self.requested_movement)
     
     def set_walkable_maze(self, walkable_maze) -> None:
+        """ Set the walkable maze matrix with a new one. """
         self._walkable_maze_matrix = walkable_maze
 
 class BFSComputer(Computer):
+    """ This class will control the character and do BFS path find. """
     def __init__(self, character, walkable_maze):
         super().__init__(character, walkable_maze)
 
     def perfrom_path_find(self):
+        """ This functions keeps searching for a path until the stop_thread flag is set. """
         while not self.stop_thread:
             self.move_based_on_path_instructions()
         print("Thread is finished")
 
     def move_based_on_path_instructions(self):
+        """ This function will get the BFS path, then  move the character to follow the
+        path it's found. """
         path_to_follow = self.bfs_path_find()
         instruction_number = 0
         target = path_to_follow[-1]
@@ -108,6 +129,13 @@ class BFSComputer(Computer):
         return None           
 
     def reconstruct_path(self, search_path_histroy, end) -> None:
+        """ The bfs algo will store store contents of every path its looked into, in this
+        function we will extract the path its found to the target.
+
+        Args:
+            search_path_histroy (list of tuples): A list of all the paths bfs has looked into.
+            end (list): The coord of the diamond.
+        """
         final_path = []
         current = end
 
@@ -120,11 +148,14 @@ class BFSComputer(Computer):
 
 
 class RandomComputer(Computer):
+    """ This class will randomly move the character around the map. """
     def __init__(self, character, walkable_maze):
         super().__init__(character, walkable_maze)
 
-    
     def perfrom_path_find(self):
+        """ This function will use the random libary to randomly select a movement for the
+        character to do, it does this by randomly updating self.requested_movement.
+        """
         # Use this flag to allow the player to descend down a ladder instead of always climbing it 
         climbing = False
 
