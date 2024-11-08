acc6f62cd1877317ca5bb18efdea48bd75696d0e
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-11-08 19:12:38 +0000

    test: mid size maze path finds are working
    I have written tests to check that mediam sized mazes are wroking
    properly with the path finding algos.

--- product/testing/test_path_finding.py
+++ product/testing/test_path_finding.py
@@ -7,11 +7,11 @@ from computer import *
 from characters import CharacterAnimationManager
 from world import World
 
+CHARACTER_WIDTH = 32 
+CHARACTER_HEIGHT = 32
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
 class TestComputer(unittest.TestCase):
-    CHARACTER_WIDTH = 32 
-    CHARACTER_HEIGHT = 32
-    SCREEN_WIDTH = 1400
-    SCREEN_HEIGHT = 800
     maze_map = [
         [1, 1, 1, 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -34,7 +34,7 @@ class TestComputer(unittest.TestCase):
     def setUp(self):
         pygame.init()
         pygame.display.set_mode((1,1), 0, 32)
-        self.player = CharacterAnimationManager(self.CHARACTER_WIDTH, self.CHARACTER_HEIGHT, self.maze_map, True, 500, 700)
+        self.player = CharacterAnimationManager(CHARACTER_WIDTH, CHARACTER_HEIGHT, self.maze_map, True, 500, 700)
         self.player.set_char_animation("idle", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Idle_4.png", 4)  
         self.player.set_char_animation("jump", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Jump_8.png", 8)
         self.player.set_char_animation("walk", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Walk_6.png", 6)
@@ -79,6 +79,74 @@ class TestComputer(unittest.TestCase):
                         [(13, 9), (12, 9), (12, 10), (12, 11), (12, 12), (12, 13), (12, 14)])        
         computer.stop_thread = True
 
+class TestComputerMidMaze(unittest.TestCase):
+    maze_map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 4, 4, 1, 3, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 4, 3, 4, 4, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 3, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 4, 4, 4, 1, 1, 3, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 2, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1],
+        [1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1],
+        [1, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 4, 4, 4, 4, 4, 4, 4, 4, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+    def setUp(self):
+        pygame.init()
+        pygame.display.set_mode((1,1), 0, 32)
+        self.player = CharacterAnimationManager(CHARACTER_WIDTH, CHARACTER_HEIGHT, self.maze_map, True, 500, 700)
+        self.player.set_char_animation("idle", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Idle_4.png", 4)  
+        self.player.set_char_animation("jump", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Jump_8.png", 8)
+        self.player.set_char_animation("walk", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Walk_6.png", 6)
+        self.player.set_char_animation("climb", r"product\assets\images\characters\Dude_Monster\Dude_Monster_Climb_4.png", 4)
+
+        self.world = World(self.maze_map)
+    
+    def tearDown(self):
+        pygame.quit()
+
+    def test_bfs_can_find_path_in_mid_maze(self):
+        computer = BFSComputer(self.player, self.world.get_walkable_maze_matrix())
+        path = computer.generate_path()
+        computer.stop_thread = True
+        self.assertEqual(path, 
+                        [(13, 9), (12, 9), (12, 8), (12, 7), (12, 6), (12, 5), (12, 4), 
+                         (11, 4), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), 
+                         (9, 9), (8, 9), (8, 10), (8, 11), (8, 12), (8, 13), (8, 14), 
+                         (8, 15), (8, 16), (8, 17), (8, 18)])        
+        computer.stop_thread = True
+    
+    def test_dfs_can_find_path_in_mid_maze(self):
+        computer = DFSComputer(self.player, self.world.get_walkable_maze_matrix())
+        computer.stop_thread = True
+        path = computer.generate_path()
+        self.assertEqual(path, 
+                        [(13, 9), (12, 9), (12, 8), (12, 7), (12, 6), (12, 5), 
+                         (12, 4), (11, 4), (10, 4), (10, 5), (10, 6), (10, 7), 
+                         (10, 8), (10, 9), (9, 9), (8, 9), (7, 9), (6, 9), (6, 8), 
+                         (6, 7), (6, 6), (6, 5), (6, 4), (5, 4), (4, 4), (4, 5), 
+                         (4, 6), (4, 7), (4, 8), (4, 9), (4, 10), (4, 11), (4, 12), 
+                         (5, 12), (6, 12), (6, 13), (6, 14), (6, 15), (6, 16), 
+                         (6, 17), (7, 17), (8, 17), (8, 18)])        
+        computer.stop_thread = True
+    
+    def test_ucs_can_find_path_in_mid_maze(self):
+        computer = UCSComputer(self.player, self.world.get_walkable_maze_matrix())
+        computer.stop_thread = True
+        path = computer.generate_path()
+        self.assertEqual(path, 
+                        [(13, 9), (12, 9), (12, 8), (12, 7), (12, 6), (12, 5), (12, 4), 
+                         (11, 4), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), 
+                         (9, 9), (8, 9), (8, 10), (8, 11), (8, 12), (8, 13), 
+                         (8, 14), (8, 15), (8, 16), (8, 17), (8, 18)])        
+        computer.stop_thread = True
 
 if __name__ == '__main__':
     unittest.main()
