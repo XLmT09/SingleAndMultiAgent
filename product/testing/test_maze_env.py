1ca39c83516c331923b5ccd5ffd975dac5582d96
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2024-12-01 01:17:16 +0000

    test: check every maze generated had a goal
    Before there was only a test that will check if maze 1 had a diamond in
    it. Now I have updated the test such that it will test every maze has a
    diamond in it.
    
    This test is needed as the algos I have coded cannot function properly
    without at least one diamond.

--- product/testing/test_maze_env.py
+++ product/testing/test_maze_env.py
@@ -5,6 +5,7 @@ from constants import player_sprite_file_paths
 import pygame
 import unittest
 import pickle
+import os
 
 CHARACTER_WIDTH = 32
 CHARACTER_HEIGHT = 32
@@ -12,10 +13,13 @@ SCREEN_WIDTH = 1400
 SCREEN_HEIGHT = 800
 # Test cases will generate the maze map
 maze_map = None
+# The directory all the mazes have been generated into
+maze_dir = "maze"
 
 
 class TestMazeEnviorment(unittest.TestCase):
-    with open('maze_1', 'rb') as file:
+    """ Test if the maze enviorment is setup as expected. """
+    with open('maze/maze_1', 'rb') as file:
         maze_map = pickle.load(file)
 
     def setUp(self):
@@ -39,14 +43,29 @@ class TestMazeEnviorment(unittest.TestCase):
     def tearDown(self):
         pygame.quit()
 
-    def test_maze_has_diamond(self):
-        diamond_found = False
-        for i in range(len(self.maze_map)):
-            for j in range(len(self.maze_map[0])):
-                if (self.maze_map[i][j] == 2):
-                    diamond_found = True
-                    break
-        self.assertEqual(True, diamond_found)
+    def test_every_maze_generated_has_a_diamond(self):
+        """ If a maze does not contain an diamond then the algos programmed
+        will not behave as expected, so we need to sure every maze generated
+        contains an diamond."""
+
+        # loop through every maze file in the maze directory
+        for file in os.listdir(maze_dir):
+            # get the path of the maze file so we can open the it
+            maze_path = os.path.join(maze_dir, file)
+
+            with open(maze_path, "rb") as maze_file:
+                # load up the maze matrix
+                temp_maze = pickle.load(maze_file)
+                diamond_found = False
+
+                # Now loop through the maze and check it contains a diamond
+                for i in range(len(temp_maze)):
+                    for j in range(len(temp_maze[0])):
+                        if (temp_maze[i][j] == 2):
+                            diamond_found = True
+                            break
+
+                self.assertEqual(True, diamond_found)
 
 
 if __name__ == '__main__':
