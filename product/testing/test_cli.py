45783640b10fdbed9aa57e73a9db20b1e1d3eadc
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2025-02-15 19:58:21 +0000

    test: cli defualt is 0 when enemy flag not given

--- product/testing/test_cli.py
+++ product/testing/test_cli.py
@@ -278,3 +278,10 @@ class TestCli(unittest.TestCase):
         """ Test checks enemy_count value given by user is registered."""
         result = process_args()
         self.assertEqual(result["enemy_count"], 3)
+
+    @patch('sys.argv', ['main', '--algo', 'ucs', '--size', 'small'])
+    def test_cli_enemy_count_default(self):
+        """ Test checks that when enemy count value is not given, then by
+        default it should be 0"""
+        result = process_args()
+        self.assertEqual(result["enemy_count"], 0)
