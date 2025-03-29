909be7a327f3c56ad17a711c24260f478d323866
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2025-03-29 18:34:46 +0000

    test: check minimax gui algo
    Check that the minimax algo works as expected in a test maze. There are
    only 5 diamonds and the test expects the main agent to get all diamonds
    before the enemy can catch them.

---
+++ product/testing/test_comp_gui.py
@@ -0,0 +1,244 @@
+import unittest
+import pygame
+import pickle
+import time
+
+from agent.competitive_computer import MinimaxComputer
+from characters.character import get_character_types
+
+from world import World
+from constants import (
+    player_sprite_file_paths,
+    game_values,
+    pink_enemy_file_sprite_paths
+)
+
+CHARACTER_WIDTH = 32
+CHARACTER_HEIGHT = 32
+
+# Maze map the functions will use
+maze_map = None
+with open('maze/test_maze_8', 'rb') as file:
+    maze_map = pickle.load(file)
+clock = pygame.time.Clock()
+
+
+class TestCompFilledGUIComputer():
+    """ Setup class which the other tests classes in this
+    file will inherit from.
+
+    Format is similar to main.py, anything not commented here would
+    likely be commented ove.r there.
+    """
+    def setUp(self, pos_x, pos_y):
+        """
+        Args:
+            pos_x (int): x position of the agent.
+            pos_y (int): y position of the agent.
+        """
+
+        # To avoid frozen screen between tests we will init the display
+        # instead of the whole pygame module.
+        pygame.display.init()
+        self.screen = pygame.display.set_mode((850, 350))
+
+        self.player = get_character_types()["main"](
+            CHARACTER_WIDTH,
+            CHARACTER_HEIGHT,
+            maze_map,
+            True,
+            pos_x,
+            pos_y,
+            in_filled_maze=True
+        )
+
+        self.player.set_char_animation(
+            "idle",
+            player_sprite_file_paths["idle"],
+            animation_steps=4
+        )
+        self.player.set_char_animation(
+            "jump",
+            player_sprite_file_paths["jump"],
+            animation_steps=8
+        )
+        self.player.set_char_animation(
+            "walk",
+            player_sprite_file_paths["walk"],
+            animation_steps=6
+        )
+        self.player.set_char_animation(
+            "climb",
+            player_sprite_file_paths["climb"],
+            animation_steps=4
+        )
+
+        self.world = World(maze_map)
+        self.game_over = 0
+        self.tile_data = self.world.get_collidable_tile_list()
+        self.diamond_positions = self.world.get_diamond_group()
+
+        self.bg = pygame.image.load(
+            "assets/images/background/cave.png"
+        ).convert_alpha()
+
+        self.enemy = get_character_types()["enemy"](
+            CHARACTER_WIDTH,
+            CHARACTER_HEIGHT,
+            maze_map,
+            is_controlled_by_computer=True,
+            x=200, y=100
+        )
+
+        self.enemy.set_char_animation(
+            "idle",
+            pink_enemy_file_sprite_paths["idle"],
+            animation_steps=4
+        )
+        self.enemy.set_char_animation(
+            "walk",
+            pink_enemy_file_sprite_paths["walk"],
+            animation_steps=6
+        )
+        self.enemy.set_char_animation(
+            "climb",
+            pink_enemy_file_sprite_paths["climb"],
+            animation_steps=4
+        )
+        self.enemy.set_char_animation(
+            "jump",
+            pink_enemy_file_sprite_paths["jump"],
+            animation_steps=8
+        )
+
+        diamond_pos = []
+
+        for dmd in self.world.get_diamond_group():
+            diamond_pos.append((dmd.grid_y, dmd.grid_x))
+
+        self.state = {
+            "main_agent": self.player.get_player_grid_coordinates(),
+            "enemies": self.enemy.get_player_grid_coordinates(),
+            "diamond_positions": diamond_pos,
+            "score": 0,
+            "win": False,
+            "lose": False,
+            "diamond_count": 0
+        }
+
+    def testPathFindGUI(self):
+        """ This function will run a given path finding algorithm in the
+        given initialized maze environment. The test passes if it can collect
+        two diamonds without errors. """
+
+        # Max time this test will be allocated (in seconds)
+        max_time = 250
+        start_time = time.time()
+
+        # Start the path finding algorithm
+        self.main_computer.start_thread()
+        self.enemy_computer.start_thread()
+
+        # if time limit exceeds this is set to false
+        running = True
+
+        # We will stop once all diamonds have been found
+        score_count = 0
+        # This test maze will only have 5 diamonds
+        TARGET = 5
+
+        # We will run a game loop until collision is detected
+        while running:
+            self.screen.blit(self.bg, (0, 0))
+
+            # Blitting the tiles
+            self.world.load_world(self.screen)
+
+            # Update diamond list present, as some may have been collected
+            dmd_list = []
+            for dmd in self.world.get_diamond_group():
+                dmd_list.append((dmd.grid_y, dmd.grid_x))
+
+            # Update the state of the game
+            new_state = {
+                "main_agent": self.player.get_player_grid_coordinates(),
+                "enemies": self.enemy.get_player_grid_coordinates(),
+                "diamond_positions": dmd_list,
+                "score": 0,
+                "win": False,
+                "lose": False,
+                "diamond_count": 0
+            }
+
+            # Moving the enemy
+            self.enemy_computer.move(
+                self.screen,
+                self.tile_data
+            )
+
+            self.enemy_computer.update_state(new_state)
+            self.main_computer.update_state(new_state)
+
+            # Moving the player
+            self.game_over, remove_diamond_pos = self.main_computer.move(
+                self.screen,
+                self.tile_data,
+                asset_groups=self.diamond_positions,
+                game_over=self.game_over,
+                enemy_computers=[self.enemy_computer]
+            )
+
+            # We have found the diamond and can begin to stop the test
+            if self.player.get_is_diamond_found():
+                self.world.clear_diamond(remove_diamond_pos[0],
+                                         remove_diamond_pos[1])
+                self.player.set_is_diamond_found_to_false()
+                self.diamond_positions = self.world.get_diamond_group()
+                self.main_computer.update_diamond_list(self.diamond_positions)
+                score_count += 1
+
+            # Can end the test once collision is detected
+            if score_count == TARGET:
+                self.assertTrue("Test passed as all diamonds are collected.")
+                self.main_computer.stop_thread = True
+                break
+
+            if time.time() - start_time > max_time:
+                running = False
+                self.assertFalse(f"Time limit of {max_time} seconds exceeded.")
+
+            # Set the game refresh rate
+            clock.tick(game_values["FPS"])
+
+            # Now render all changes we made in this loop
+            # iteration onto the game screen.
+            pygame.display.update()
+
+    def tearDown(self):
+        self.main_computer.stop_thread = True
+        self.enemy_computer.stop_thread = True
+        pygame.quit()
+
+
+class TestMinimaxGUIComputer(TestCompFilledGUIComputer, unittest.TestCase):
+    """ This tests the minimax computer class. It will check the main agent is
+    able to collect all diamonds in the maze, this should be possible as there
+    are no dead ends in the game."""
+
+    def setUp(self):
+        super().setUp(pos_x=350, pos_y=300)
+        self.main_computer = MinimaxComputer(
+            self.player,
+            self.world.get_walkable_maze_matrix(),
+            diamond_list=self.world.get_diamond_group(),
+            is_weighted=True,
+            enemy_list=[self.enemy],
+            state=self.state,
+            is_main=True
+        )
+
+        self.enemy_computer = MinimaxComputer(
+            self.enemy,
+            self.world.get_walkable_maze_matrix(),
+            state=self.state
+        )
