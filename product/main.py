44f65e62f1501910349e67c19ac3c0a62ad7aad9
Hijithan Arunthavarajah <zjac082@live.rhul.ac.uk>
2025-04-02 19:01:43 +0100

    chore: move cli logic into its own file
    main.py is getting too big, one reason is because of the parser code so
    I moved it to a new file.

--- product/main.py
+++ product/main.py
@@ -1,6 +1,5 @@
 import pygame
 import pickle
-import argparse
 import time
 import os
 
@@ -9,6 +8,7 @@ from agent.computer import get_agent_types
 from characters.character import get_character_types
 from text import Text
 from lock import visited_and_path_data_flag
+from cli import process_args
 
 import constants as C
 
@@ -17,138 +17,6 @@ import constants as C
 pygame.init()
 clock = pygame.time.Clock()
 pygame.display.set_caption("Maze Game")
-
-
-def process_args() -> dict:
-    """ This function takes the flags passed in by the user and
-    will process them.
-    """
-
-    # This is used to define, manage and parser the command line args.
-    parser = argparse.ArgumentParser()
-
-    # define the size flag
-    parser.add_argument(
-        "--size",
-        choices=[
-            "small", "medium", "large",
-            "small-filled", "medium-filled", "large-filled"
-        ],
-        required=True,
-        help="Choose a size: small, medium, or large"
-    )
-
-    # define the algo flag
-    parser.add_argument(
-        "--algo",
-        choices=["random", "dfs", "bfs", "ucs", "astar", "greedy", "minimax"],
-        required=False,
-        help="Choose a algorithm: random, dfs, bfs, or ucs"
-    )
-
-    parser.add_argument(
-        "--weighted",
-        action="store_true",
-        help="Use a weighted manhattan (x2) to influence the "
-             "pathfinding (only applicable to A*)."
-    )
-
-    # define the algo flag
-    parser.add_argument(
-        "--highlight",
-        action="store_true",
-        required=False,
-        help="Highlights the visited grids and final path before moving the"
-             " agent"
-    )
-
-    parser.add_argument(
-        "--enemy_count",
-        type=int,
-        default=0,
-        required=False,
-        help="The number of enemies to appear in the game."
-    )
-
-    # parse args from command line
-    args = parser.parse_args()
-
-    screen_width, screen_height, maze, filled = None, None, None, False
-
-    # setup the window width and height, depending on
-    # the size the user specified.
-    if "small" in args.size:
-        screen_width = 850
-        screen_height = 350
-        # retrieve the small maze filled with diamonds
-        if "filled" in args.size:
-            if args.algo == "minimax":
-                maze = "maze/maze_8"
-            else:
-                maze = "maze/maze_5"
-            filled = True
-        else:
-            maze = "maze/maze_1"
-    elif "medium" in args.size:
-        screen_width = 1000
-        screen_height = 750
-        if "filled" in args.size:
-            maze = "maze/maze_6"
-            filled = True
-        else:
-            maze = "maze/maze_2"
-    elif "large" in args.size:
-        screen_width = 1400
-        screen_height = 750
-        if "filled" in args.size:
-            maze = "maze/maze_7"
-            filled = True
-        else:
-            maze = "maze/maze_3"
-
-    if args.weighted and args.algo != "astar":
-        parser.error("--weighted is only applicable when using the "
-                     "A* algorithm.")
-
-    if ((not args.algo and args.highlight) or
-       (args.algo == "random" and args.highlight)):
-        parser.error(
-            "--highlight is only applicable when using any algorithm but "
-            "random."
-        )
-
-    filled_compatible_algos = ["greedy", "random", "astar", "minimax"]
-    if (args.algo and filled and not (args.algo in filled_compatible_algos)):
-        parser.error(
-            "Filled maze only works when user controlled or when using "
-            "greedy algorithm."
-        )
-
-    if args.algo == "minimax" and args.enemy_count == 0:
-        parser.error(
-            "Minimax algorithm requires at least one enemy to be present."
-        )
-
-    if args.enemy_count > C.MAX_ENEMIES:
-        parser.error(
-            f"Enemy count cannot be greater than {C.MAX_ENEMIES}."
-        )
-
-    # If we are in a filled maze and using astar then update astar to work in
-    # a filled maze context.
-    if filled and args.algo == "astar":
-        args.algo = "astarFilled"
-
-    return {
-        "maze_path": maze,
-        "screen_width": screen_width,
-        "screen_height": screen_height,
-        "algo": args.algo,
-        "enable_highlighter": args.highlight,
-        "weighted": args.weighted,
-        "filled": filled,
-        "enemy_count": args.enemy_count
-    }
 
 
 def create_characters(config, maze_array) -> list:
